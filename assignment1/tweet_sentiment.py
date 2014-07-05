bc2502a62802809ea2c4a7a74f53f39ad7f3d2bd
Cui Li <cui.judy.lee@gmail.com>
2014-07-05 12:07:13 +0800

    finish homework for tweet sentitment

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -1,17 +1,40 @@
 import sys
+import json
+import re
+scores = {}
 
-def hw():
-    print 'Hello, world!'
+def load_dict(sent_file):
+    afinnfile = open(sent_file,'r')
+    for line in afinnfile:
+        (term, score) = line.split('\t')
+        scores[term]=int(score)
+    afinnfile.close()
 
-def lines(fp):
-    print str(len(fp.readlines()))
+def cal_score(tweet_file):
+    tf = open(tweet_file,'r')
+    for line in tf:
+        score = 0
+	j = json.loads(line)
+	if 'text' in j.keys():  #tweets
+            text = j['text']
+            print(text)
+            for w in text.split(' '):
+                w = w.lower()
+                match = re.search('\w+',w)
+                if match:
+                    w = match.group()
+                    print(w)
+                    if w in scores.keys():
+                        score+=scores[w]
+            print(score)
+    tf.close()
 
 def main():
-    sent_file = open(sys.argv[1])
-    tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+    load_dict(sys.argv[1])
+    cal_score(sys.argv[2])
+    #hw()
+    #lines(sent_file)
+    #lines(tweet_file)
 
 if __name__ == '__main__':
     main()
